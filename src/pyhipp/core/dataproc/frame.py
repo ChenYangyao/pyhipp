b137b480482c71fc5777335f07f437bf883cc4d0
Yangyao Chen <yangyaochen.astro@foxmail.com>
2023-08-19 01:16:17 +0800

    add "cart" conversion methods

--- src/pyhipp/core/dataproc/frame.py
+++ src/pyhipp/core/dataproc/frame.py
@@ -1,6 +1,7 @@
 import numpy as np
+from .num import Num
 
-class Polor:
+class Polar:
     @staticmethod
     def unit_vec_to_cart(theta: np.ndarray, phi: np.ndarray, stack=True):
         cos_t, sin_t = np.cos(theta), np.sin(theta)
@@ -12,6 +13,34 @@ class Polor:
         if stack:
             out = np.stack(out, axis=-1)
         return out
+    
+    @staticmethod
+    def unit_vec_to_cart_2d(theta: np.ndarray, stack=True):
+        out = np.cos(theta), np.sin(theta)
+        if stack:
+            out = np.stack(out, axis=-1)
+        return out
+    
+    @staticmethod
+    def rt_to_cart(x: np.ndarray, v: np.ndarray) -> np.ndarray:
+        '''
+        Convert radial-tangential coordinates to cartesian. Returns (n,3) 
+        array-like, the cartesian coordinates.
+        
+        @x: (n,3) array-like, spatial position.
+        @v: (n,3) array-like, vector to be transformed.
+        '''
+        vnew_z = np.array(x)
+        vold_z = np.array([0,0,1])
+        vnew_x = np.cross(vold_z, vnew_z)
+        vnew_y = np.cross(vnew_z, vnew_x)
+
+        vnew_x = Num.normalize(vnew_x, inplace=True)
+        vnew_y = Num.normalize(vnew_y, inplace=True)
+        vnew_z = Num.normalize(vnew_z, inplace=True)
+
+        v = np.asarray(v)
+        return vnew_x*v[:,[0]]+vnew_y*v[:,[1]]+vnew_z*v[:,[2]]
     
 class Cartesian:
     pass
