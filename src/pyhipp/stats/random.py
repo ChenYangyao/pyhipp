7a9e928236dfb3450f87e8d992010fcdac5370ab
Yangyao Chen <yangyaochen.astro@foxmail.com>
2024-06-08 15:57:09 +0800

    add jit rng

--- src/pyhipp/stats/random.py
+++ src/pyhipp/stats/random.py
@@ -3,6 +3,57 @@ from numpy import random as npr
 from typing import Union
 import numpy as np
 from ..core.dataproc.frame import Polar
+import numba
+from numba.experimental import jitclass
+
+_spec = [
+    ('_np_rng', numba.typeof(np.random.default_rng(0))),
+]
+_NpRng = np.random.Generator
+
+@jitclass(_spec)
+class _Rng:
+    def __init__(self, np_rng: _NpRng) -> None:
+        '''
+        @np_rng: numpy.random.Generator.
+        '''
+        self._np_rng = np_rng
+        
+    def random(self, size=None):
+        '''Uniform floating point value in [0, 1).'''
+        return self._np_rng.random(size=size)
+    
+    def uniform(self, low=0.0, high=1.0, size=None):
+        '''Uniform floating point value in [low, high).'''
+        return self._np_rng.uniform(low=low, high=high, size=size)
+
+    def standard_normal(self, size=None):
+        return self._np_rng.standard_normal(size=size)
+
+    def normal(self, loc = 0.0, scale = 1.0, size = None):
+        return self._np_rng.normal(loc = loc, scale = scale, size = size)
+    
+    def uniform_sphere_polar(self, size=None):
+        '''
+        Return theta, phi.
+        '''
+        cos_theta = self.uniform(-1., 1., size=size)
+        phi = self.uniform(0., 2.0*np.pi, size=size)   # [0, 2 pi]
+        theta = np.arccos(cos_theta)                   # [0, pi]
+        
+        return theta, phi
+        
+    def uniform_sphere_cart(self, size=None):
+        '''
+        Return x, y, z.
+        '''
+        theta, phi = self.uniform_sphere_polar(size=size)
+        cos_t, sin_t = np.cos(theta), np.sin(theta)
+        cos_p, sin_p = np.cos(phi), np.sin(phi)
+        z = cos_t
+        x = sin_t * cos_p
+        y = sin_t * sin_p
+        return x, y, z
 
 class Rng:
     
